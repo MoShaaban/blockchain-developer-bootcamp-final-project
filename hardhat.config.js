65883e38e8e36ec39c9c53e5ae97eaed258aeaa5
Mo Behairy <m.shaaban101@gmail.com>
2021-12-12 23:26:55 -0800

    smart contract and frontend work as intended

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,6 +17,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.0",
+ module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/FmOuuLRNu4IZgEprYm9vPKHRbLdm51k8',
+      accounts: [''],
+    },
+  },
+  etherscan: {
+    apiKey: ""
+  }
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('MyPhotographyNFTs');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to:", nftContract.address);
+  
+
+  //Call the makeNFT function
+  let txn = await nftContract.makeNFT()
+  //wait for the NFT to be mined
+  await txn.wait()
+  console.log("Minted NFT #1")
+};
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+
+  runMain();
+  
